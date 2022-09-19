2b156cc67cdb9a7ad058afdb9db73f8e0cba4ebc
D.Tadpole <gsk390@gmail.com>
2022-09-19 04:05:59 -0700

    info

--- mnist.jl
+++ mnist.jl
@@ -37,7 +37,7 @@ x_train, y_train = trainset[:]
 const len_train = length(y_train)
 x_train_ = reshape(x_train, 28, 28, 1, :)
 y_train_ = convert(Array{Float32}, onehotbatch(y_train, 0:9))
-# @info "Train data" typeof(trainset[1]) size(trainset[1]) typeof(trainset[2]) size(trainset[2])
+@info "Train data" typeof(x_train_) size(x_train_) typeof(y_train_) size(y_train_)
 
 ##################################################
 # Test dataset
@@ -46,7 +46,7 @@ testset = MNIST.testdata()
 x_test, y_test = trainset[:]
 x_test_ = reshape(x_test, 28, 28, 1, :)
 y_test_ = convert(Array{Float32}, onehotbatch(y_test, 0:9))
-# @info "Test data" typeof(trainset[1]) size(trainset[1]) typeof(trainset[2]) size(trainset[2])
+@info "Test data" typeof(x_test_) size(x_test_) typeof(y_test_) size(y_test_)
 
 
 ##################################################
@@ -54,10 +54,10 @@ y_test_ = convert(Array{Float32}, onehotbatch(y_test, 0:9))
 model = Chain(
     Conv((3, 3), 1 => 8, relu, pad=(1, 1)),
     Conv((3, 3), 8 => 16, relu, pad=(1, 1)),
-    Conv((3, 3), 16 => 32, relu, pad=(1, 1)),
-    x -> reshape(x, (28 * 28 * 32, :)),
+    Conv((3, 3), 16 => 16, relu, pad=(1, 1)),
+    x -> reshape(x, (28 * 28 * 16, :)),
     Dropout(0.4),
-    Dense(28 * 28 * 32 => 256, elu),
+    Dense(28 * 28 * 16 => 256, elu),
     Dropout(0.4),
     Dense(256 => 10, elu)
 )
@@ -102,8 +102,8 @@ function train()
     for epoch in 1:10
         # shuffle training data
         s = shuffle(1:len_train) # s = 1:len_train
-        x_train_s = copy(x_train_[:, :, :, s])
-        y_train_s = copy(y_train_[:, s])
+        x_train_s = x_train_[:, :, :, s]
+        y_train_s = y_train_[:, s]
         # train batch
         for i in 1:BATCH_SIZE:len_train
             x = x_train_s[:, :, :, i:i+BATCH_SIZE-1]
