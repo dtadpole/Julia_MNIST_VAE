f845a57d6bedea7f5ecf469e85329de1bd3ad48e
D.Tadpole <gsk390@gmail.com>
2022-09-19 03:01:39 -0700

    info

--- mnist.jl
+++ mnist.jl
@@ -6,6 +6,8 @@ using Random
 using OneHotArrays
 using StatsBase
 
+##################################################
+# Parse command line arguments
 function parse_commandline()
 
     s = ArgParseSettings()
@@ -26,16 +28,18 @@ if args["model_cuda"] >= 0
     CUDA.device!(args["model_cuda"])
 end
 
-# train data
+##################################################
+# Training dataset
 trainset = MNIST.traindata()
 
 x_train, y_train = trainset[:]
-len_train = length(y_train)
+const len_train = length(y_train)
 x_train_ = reshape(x_train, 28, 28, 1, :)
 y_train_ = convert(Array{Float32}, onehotbatch(y_train, 0:9))
 # @info "Train data" typeof(trainset[1]) size(trainset[1]) typeof(trainset[2]) size(trainset[2])
 
-# test data
+##################################################
+# Test dataset
 testset = MNIST.testdata()
 
 x_test, y_test = trainset[:]
@@ -47,59 +51,79 @@ if args["model_cuda"] >= 0
     y_test_ = y_test_ |> gpu
 end
 
+##################################################
+# training
+function train()
 
-model = Chain(
-    Conv((3, 3), 1 => 4, relu, pad=(1, 1), stride=(1, 1)),
-    Conv((3, 3), 4 => 8, relu, pad=(1, 1), stride=(1, 1)),
-    x -> reshape(x, (28 * 28 * 8, :)),
-    # Dropout(0.5),
-    Dense(28 * 28 * 8 => 64, elu),
-    # Dropout(0.5),
-    Dense(64 => 10, elu),
-    softmax
-)
-if args["model_cuda"] >= 0
-    model = model |> gpu
-end
+    global len_train
+    global x_train_
+    global y_train_
+    global x_test_
+    global y_test_
 
-lossF = (x, y) -> begin
-    y_ = model(x)
-    return mean(-sum(y .* log.(y_), dims=1))
-end
-
-accuracy = (x_, y_) -> mean(argmax(model(x_) |> cpu, dims=1) .== argmax(y_ |> cpu, dims=1))
-
-@info "Before training" round(accuracy(x_test_, y_test_), digits=3)
-
-opt = ADAM(0.01)
-# opt = AdamW(0.01, (0.9, 0.999), 0.0001)
-if args["model_cuda"] >= 0
-    opt = opt |> gpu
-end
-
-params = Flux.params(model)
-
-BATCH_SIZE = 100
-for epoch in 1:10
-    # shuffle training data
-    # s = shuffle(1:len_train)
-    s = 1:len_train
-    x_train_s = x_train_[:, :, :, s]
-    y_train_s = y_train_[:, s]
-    # train batch
-    for i in 1:BATCH_SIZE:len_train
-        x = x_train_s[:, :, :, i:i+BATCH_SIZE-1]
-        y = y_train_s[:, i:i+BATCH_SIZE-1]
-        if args["model_cuda"] >= 0
-            x = x |> gpu
-            y = y |> gpu
-        end
-        grads = gradient(() -> lossF(x, y), params)
-        Flux.update!(opt, params, grads)
+    model = Chain(
+        Conv((3, 3), 1 => 4, relu, pad=(1, 1), stride=(1, 1)),
+        Conv((3, 3), 4 => 8, relu, pad=(1, 1), stride=(1, 1)),
+        x -> reshape(x, (28 * 28 * 8, :)),
+        # Dropout(0.5),
+        Dense(28 * 28 * 8 => 64, elu),
+        # Dropout(0.5),
+        Dense(64 => 10, elu),
+        softmax
+    )
+    if args["model_cuda"] >= 0
+        model = model |> gpu
     end
-    @info "Train epoch" epoch round(accuracy(x_train_, y_train_), digits=3)
+
+    lossF = (x, y) -> begin
+        y_ = model(x)
+        return mean(-sum(y .* log.(y_), dims=1))
+    end
+
+    accuracy = (x_, y_) -> mean(argmax(model(x_) |> cpu, dims=1) .== argmax(y_ |> cpu, dims=1))
+
+    @info "Before training" round(accuracy(x_train_, y_train_), digits=3) round(accuracy(x_test_, y_test_), digits=3)
+
+    opt = ADAM(0.01)
+    # opt = AdamW(0.01, (0.9, 0.999), 0.0001)
+    if args["model_cuda"] >= 0
+        opt = opt |> gpu
+    end
+
+    params = Flux.params(model)
+
+    BATCH_SIZE = 100
+    for epoch in 1:10
+        # shuffle training data
+        # s = shuffle(1:len_train)
+        s = 1:len_train
+        x_train_s = x_train_[:, :, :, s]
+        y_train_s = y_train_[:, s]
+        # train batch
+        for i in 1:BATCH_SIZE:len_train
+            x = x_train_s[:, :, :, i:i+BATCH_SIZE-1]
+            y = y_train_s[:, i:i+BATCH_SIZE-1]
+            if args["model_cuda"] >= 0
+                x = x |> gpu
+                y = y |> gpu
+            end
+            grads = gradient(() -> lossF(x, y), params)
+            Flux.update!(opt, params, grads)
+        end
+        @info "Train epoch" epoch round(accuracy(x_train_, y_train_), digits=3) round(accuracy(x_test_, y_test_), digits=3)
+    end
 end
 
-# run test
-testmode!(model)
-@info "Test result" round(accuracy(x_test_, y_test_), digits=3)
+##################################################
+# test
+function test()
+    global x_test_
+    global y_test_
+    # run test
+    testmode!(model)
+    @info "Test result" round(accuracy(x_test_, y_test_), digits=3)
+end
+
+train()
+
+# test()
