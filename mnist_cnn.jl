5e3c8140d4fac48849d87a034cef6de6895378cf
D.Tadpole <gsk390@gmail.com>
2022-09-19 16:42:51 -0700

    model use max pool

--- mnist_cnn.jl
+++ mnist_cnn.jl
@@ -12,72 +12,77 @@ using StatsBase
 # Training dataset
 trainset = MNIST.traindata()
 
-x_train, y_train = trainset[:]
-LEN_TRAIN = length(y_train)
-x_train_cpu = convert(Array{Float32}, reshape(x_train, 28, 28, 1, :))
-y_train_cpu = convert(Array{Float32}, onehotbatch(y_train, 0:9))
-if args["model_cuda"] >= 0
-    x_train_ = x_train_cpu |> gpu
-    y_train_ = y_train_cpu |> gpu
-else
-    x_train_ = x_train_cpu
-    y_train_ = y_train_cpu
-end
+x_train_, y_train_ = trainset[:]
+x_train_ = convert(Array{Float32}, reshape(x_train_, 28, 28, 1, :))
+y_train_ = convert(Array{Float32}, onehotbatch(y_train_, 0:9))
 @info "Train data" typeof(x_train_) size(x_train_) typeof(y_train_) size(y_train_)
 
+TRAIN_LENGTH = size(y_train_, 2)
 
 ##################################################
 # Test dataset
 testset = MNIST.testdata()
 
-x_test, y_test = testset[:]
-x_test_cpu = convert(Array{Float32}, reshape(x_test, 28, 28, 1, :))
-y_test_cpu = convert(Array{Float32}, onehotbatch(y_test, 0:9))
-if args["model_cuda"] >= 0
-    x_test_ = x_test_cpu # |> gpu
-    y_test_ = y_test_cpu # |> gpu
-else
-    x_test_ = x_test_cpu
-    y_test_ = y_test_cpu
-end
+x_test_, y_test_ = testset[:]
+x_test_ = convert(Array{Float32}, reshape(x_test_, 28, 28, 1, :))
+y_test_ = convert(Array{Float32}, onehotbatch(y_test_, 0:9))
 @info "Test data" typeof(x_test_) size(x_test_) typeof(y_test_) size(y_test_)
 
 
 ##################################################
-# Model
-model = Chain(
-    Conv((3, 3), 1 => 8, relu, pad=(1, 1)),
-    Conv((3, 3), 8 => 16, relu, pad=(1, 1)),
-    Conv((3, 3), 16 => 16, relu, pad=(1, 1)),
-    x -> reshape(x, (28 * 28 * 16, :)),
-    Dropout(0.5),
-    Dense(28 * 28 * 16 => 128, elu),
-    Dropout(0.5),
-    Dense(128 => 10, elu)
-)
-if args["model_cuda"] >= 0
-    model = model |> gpu
-end
-
-lossF = (x, y) -> begin
-    y_ = softmax(model(x), dims=1)
-    return mean(-sum(y .* log.(y_), dims=1))
-end
-
-accuracy = (x_, y_; test_mode=false) -> begin
-    # accuracy on cpu
-    model_cpu = model |> cpu
-    if test_mode
-        testmode!(model_cpu)
+# returns a function that returns the model
+modelF = (() -> begin
+    model = Chain(
+        Conv((3, 3), 1 => 32, relu, pad=(1, 1)),
+        MaxPool((2, 2)),
+        Conv((3, 3), 32 => 32, relu, pad=(1, 1)),
+        MaxPool((2, 2)),
+        Conv((3, 3), 32 => 32, relu, pad=(1, 1)),
+        x -> reshape(x, (7 * 7 * 32, :)),
+        # Dropout(0.5),
+        Dense(7 * 7 * 32 => 128, elu),
+        Dropout(0.5),
+        Dense(128 => 10, elu),
+        softmax
+    )
+    # model to GPU if available
+    if args["model_cuda"] >= 0
+        model = model |> gpu
     end
-    acc = mean(argmax(model_cpu(x_ |> cpu), dims=1) .== argmax(y_ |> cpu, dims=1))
+    # @info "Model" model
+    # return a function that returns the model
+    return () -> model
+end)()
+
+# return a function that returns loss function
+lossF = (model_, x_, y_) -> begin
+    y_pred = model_(x_)
+    mean(-sum(y_ .* log.(y_pred), dims=1))
+end
+
+# accuracy function
+accuracy = (model_, x_, y_; test_mode=false) -> begin
+    # accuracy on cpu
+    if args["model_cuda"] >= 0
+        model_ = model_ |> cpu
+        x_ = x_ |> cpu
+        y_ = y_ |> cpu
+    end
+    if test_mode
+        testmode!(model_)
+    end
+    acc = mean(argmax(model_(x_), dims=1) .== argmax(y_, dims=1))
     return round(acc, digits=3)
 end
 
 
 ##################################################
 # training
-function train(model)
+function train()
+
+    # get model
+    model_ = modelF()
+    @info "Model" model_
 
     # opt = ADAM(0.01)
     opt = AdamW(0.001, (0.9, 0.999), 0.0001)
@@ -85,24 +90,28 @@ function train(model)
         opt = opt |> gpu
     end
 
-    params = Flux.params(model)
+    params = Flux.params(model_)
 
-    @info "Before training" accuracy(x_train_cpu, y_train_cpu) accuracy(x_test_cpu, y_test_cpu)
+    @info "Before training" accuracy(model_, x_train_, y_train_) accuracy(model_, x_test_, y_test_)
 
     BATCH_SIZE = 100
     for epoch in 1:10
         # shuffle training data
-        s = shuffle(1:LEN_TRAIN) # s = 1:len_train
+        s = shuffle(1:TRAIN_LENGTH) # s = 1:len_train
         x_train_s = x_train_[:, :, :, s]
         y_train_s = y_train_[:, s]
         # train batch
-        for i in 1:BATCH_SIZE:LEN_TRAIN
+        for i in 1:BATCH_SIZE:TRAIN_LENGTH
             x = x_train_s[:, :, :, i:i+BATCH_SIZE-1]
             y = y_train_s[:, i:i+BATCH_SIZE-1]
-            grads = gradient(() -> lossF(x, y), params)
+            if args["model_cuda"] >= 0
+                x = x |> gpu
+                y = y |> gpu
+            end
+            grads = gradient(() -> lossF(model_, x, y), params)
             Flux.update!(opt, params, grads)
         end
-        @info "Train epoch" epoch accuracy(x_train_cpu, y_train_cpu) accuracy(x_test_cpu, y_test_cpu)
+        @info "Train epoch" epoch accuracy(model_, x_train_, y_train_) accuracy(model_, x_test_, y_test_)
         if args["model_cuda"] >= 0
             CUDA.reclaim()
         end
@@ -112,19 +121,18 @@ end
 
 ##################################################
 # Test
-function test(model)
-
-    # run test
-    @info "Test result" accuracy(x_test_cpu, y_test_cpu, test_mode=true)
+function test()
+    model_ = modelF()
+    @info "Test result" accuracy(model_, x_test_, y_test_, test_mode=true)
 end
 
 
 ##################################################
 # Main
 if abspath(PROGRAM_FILE) == @__FILE__
-    train(model)
+    train()
     if args["model_cuda"] >= 0
         CUDA.reclaim()
     end
-    test(model)
+    test()
 end
