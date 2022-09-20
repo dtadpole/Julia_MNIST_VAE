da51844bf492dba4348ec74adf0274dbf791aacb
D.Tadpole <gsk390@gmail.com>
2022-09-20 01:05:08 -0700

    use cross entropy for loss

--- mnist_vae.jl
+++ mnist_vae.jl
@@ -118,10 +118,12 @@ end
 # return a function that returns loss function
 lossF = (model_, x_) -> begin
     x_pred, mu, log_var = model_(x_)
-    loss_reconstruction = sum((x_ - x_pred) .^ 2, dims=1:2) # / (size(x_, 1) * size(x_, 2))
+    # loss_reconstruction = sum((x_ - x_pred) .^ 2, dims=1:2) # / (size(x_, 1) * size(x_, 2))
+    loss_reconstration = mean(sum(x_ .* log.(x_pred) .+ (1 .- x_) .* log.(1 .- x_pred), dims=1:2))
     # loss_kl = sum(log.(log_var / 1.0) + (1.0^2 + (mu - 0.0)^2) / (2 * log_var^2) - 0.5, dims=1)
-    loss_kl = -0.5f0 * sum(1.0f0 .+ log_var .- mu .^ 2 .- exp.(log_var), dims=1)
-    return mean(loss_reconstruction .+ loss_kl), mean(loss_reconstruction), mean(loss_kl)
+    loss_kl = mean(-0.5f0 * sum(1.0f0 .+ log_var .- mu .^ 2 .- exp.(log_var), dims=1))
+    loss = loss_reconstration + loss_kl
+    return loss, loss_reconstruction, loss_kl
 end
 
 lossF_sample = (model_, x_, size_::Int=2_000) -> begin
