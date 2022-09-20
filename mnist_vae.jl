f810d9a2669c02a06de702e6b33c910824723561
D.Tadpole <gsk390@gmail.com>
2022-09-20 01:08:19 -0700

    logit loss

--- mnist_vae.jl
+++ mnist_vae.jl
@@ -119,7 +119,7 @@ end
 lossF = (model_, x_) -> begin
     x_pred, mu, log_var = model_(x_)
     # loss_reconstruction = sum((x_ - x_pred) .^ 2, dims=1:2) # / (size(x_, 1) * size(x_, 2))
-    loss_reconstration = mean(sum(x_ .* log.(x_pred) .+ (1 .- x_) .* log.(1 .- x_pred), dims=1:2))
+    loss_reconstruction = mean(sum(x_ .* log.(x_pred) .+ (1 .- x_) .* log.(1 .- x_pred), dims=1:2))
     # loss_kl = sum(log.(log_var / 1.0) + (1.0^2 + (mu - 0.0)^2) / (2 * log_var^2) - 0.5, dims=1)
     loss_kl = mean(-0.5f0 * sum(1.0f0 .+ log_var .- mu .^ 2 .- exp.(log_var), dims=1))
     loss = loss_reconstration + loss_kl
