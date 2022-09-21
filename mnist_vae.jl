00ccdf6168cf82e6f9d3f1a100683c72dad839cd
D.Tadpole <gsk390@gmail.com>
2022-09-20 18:59:39 -0700

    working version

--- mnist_vae.jl
+++ mnist_vae.jl
@@ -57,13 +57,13 @@ create_vae = (dim_1::Int, dim_2::Int, channel_n::Int, latent_n::Int) -> begin
             Split(
                 Chain(
                     # Dropout(0.5),
-                    # Dense(channel_n * 8 => channel_n * 8, relu),
+                    Dense(channel_n * 8 => channel_n * 8, relu),
                     # Dropout(0.5),
                     Dense(channel_n * 8 => latent_n),  # mu : mean -- IMPORTANT : no activation function !!!
                 ),
                 Chain(
                     # Dropout(0.5),
-                    # Dense(channel_n * 8 => channel_n * 8, relu),
+                    Dense(channel_n * 8 => channel_n * 8, relu),
                     # Dropout(0.5),
                     Dense(channel_n * 8 => latent_n),  # sigma : log_var -- IMPORTANT : no activation function !!!
                 ),
@@ -102,7 +102,8 @@ create_vae = (dim_1::Int, dim_2::Int, channel_n::Int, latent_n::Int) -> begin
             Dense(latent_n => channel_n * 8, relu),
             # Dropout(0.5),
             Dense(channel_n * 8 => channel_n * 8, relu),
-            # Dense(channel_n * 8 => channel_n * 8, relu),
+            # Dropout(0.5),
+            Dense(channel_n * 8 => channel_n * 8, relu),
             # Dropout(0.5),
             Dense(channel_n * 8 => dim_1 * dim_2, sigmoid),
             Reshape(dim_1, dim_2, 1, :),
@@ -142,7 +143,7 @@ lossF = (encoder, decoder, x_) -> begin
     loss_kl = mean(-0.5f0 * sum(1.0f0 .+ log_var .- mu .^ 2 .- exp.(log_var), dims=1))
     # loss_kl = 0
     loss = loss_reconstruction + loss_kl
-    return loss # , loss_reconstruction, loss_kl
+    return loss # , loss_reconstruction, loss_kl, mean(mu, dims=2), mean(log_var, dims=2)
 end
 
 lossF_sample = (encoder, decoder, x_, size_::Int=2_000) -> begin
@@ -180,7 +181,7 @@ function train()
 
     function train_epoch(epoch::Int)
         # total loss
-        total_loss = 0.0f0
+        loss_total = 0.0f0
         # progress tracker
         progress_tracker = Progress(length(data_loader), 1, "Training epoch $(epoch): ")
         for (x_, y_) in data_loader
@@ -188,25 +189,34 @@ function train()
                 if args["model_cuda"] >= 0
                     x_ = x_ |> gpu
                 end
-                loss, back = pullback(params) do
+                #(loss_curr, loss_recon, loss_kl, mu, log_var)
+                loss_curr, back = pullback(params) do
                     lossF(encoder_, decoder_, x_)
                 end
                 gradients = back(1.0f0)
                 Flux.update!(opt, params, gradients)
-                next!(progress_tracker; showvalues=[(:loss, loss)])
-                total_loss += loss
+                # update progress tracker
+                loss_total += loss_curr
+                loss_avg = loss_total / (progress_tracker.counter + 1)
+                next!(progress_tracker; showvalues=[
+                    (:loss, round(loss_avg, digits=2)),
+                    (:curr, round(loss_curr, digits=2)),
+                    # (:recon, round(loss_recon, digits=2)),
+                    # (:kl, round(loss_kl, digits=2)),
+                    # (:mu, round.(mu, digits=2)),
+                    # (:log_var, round.(log_var, digits=2)),
+                ])
             end)()
             # reclaim GPU memory
-            # if mod(i, BATCH_SIZE * 50) == 1
-            #     # @show mean(lossVector) # size(lossVector)
-            #     # lossVector = Vector{Float32}()
+            # if mod(progress_tracker.counter, 20) == 0
             #     GC.gc(true)
             #     if args["model_cuda"] >= 0
             #         CUDA.reclaim()
             #     end
             # end
         end
-        return total_loss / length(data_loader)
+        # return epoch loss
+        return loss_total / length(data_loader)
     end
 
     start_time = time()
@@ -221,11 +231,8 @@ function train()
     end
 
     for epoch in 1:args["train_epochs"]
-        # start time
-        start_time = time()
         # train epoch
-        loss_epoch = train_epoch(epoch)
-        train_time = round(time() - start_time, digits=1)
+        train_epoch(epoch)
         # GC and reclaim GPU memory
         GC.gc(true)
         if args["model_cuda"] >= 0
@@ -233,9 +240,9 @@ function train()
         end
         # calculate accuracy
         start_time = time()
-        loss_test = lossF_sample(encoder_, decoder_, x_test_)
-        loss_time = round(time() - start_time, digits=1)
-        @info "[$(train_time)s] Train epoch [$(epoch)]" loss_time loss_epoch loss_test
+        loss_test = round(lossF_sample(encoder_, decoder_, x_test_), digits=2)
+        test_time = round(time() - start_time, digits=1)
+        @info "Epoch [$(epoch)] : test loss [$(loss_test)] [$(test_time)]"
         # GC and reclaim GPU memory
         GC.gc(true)
         if args["model_cuda"] >= 0
